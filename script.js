def050e0353a618a786138ba9dfc96e756e04d22
Rohan Ambhore <142285944+rohan1207@users.noreply.github.com>
2025-02-11 18:07:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let button=document.querySelector("#button");
+
+button.addEventListener("click",()=>{
+	let name=document.querySelector("#name").value;
+	let lastName=document.querySelector("#lastname").value;
+	let phone=document.querySelector("#phone").value;
+	let email=document.querySelector("#email").value;
+
+	alert(`First Name: ${name} Last Name: ${ lastName} Phone Number: ${phone} ID: ${email}`)
+	
+})
